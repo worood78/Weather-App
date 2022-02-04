e15cbf597f1f1d7160f1bd8a6ebd64c98070a94a
worood78 <woroodsf555@gmail.com>
2022-02-04 23:49:02 +0100

    city variable added and some unneeded lines removed

--- src/scripts.js
+++ src/scripts.js
@@ -27,22 +27,20 @@ let dayTime = document.querySelector("#date");
 dayTime.innerHTML = formatDate(date);
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  // let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  //dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "b9c2f1ae4e743c1d744622e272fab8ed";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&appid=${apiKey}&units=metric`;
+let city = "Amsterdam";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
