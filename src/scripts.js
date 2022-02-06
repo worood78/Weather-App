1c4e4a1b379dce03ef9c7d6a42d005a3b54da216
worood78 <woroodsf555@gmail.com>
2022-02-06 20:23:50 +0100

    Added the search engine

--- src/scripts.js
+++ src/scripts.js
@@ -44,8 +44,19 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "b9c2f1ae4e743c1d744622e272fab8ed";
-let city = "Amsterdam";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "b9c2f1ae4e743c1d744622e272fab8ed";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("Amsterdam");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
