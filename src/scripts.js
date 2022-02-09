88678de79c9195985aa8ad4717b746826c406fe3
worood78 <woroodsf555@gmail.com>
2022-02-09 14:34:05 +0100

    started working on forecast section

--- src/scripts.js
+++ src/scripts.js
@@ -26,6 +26,35 @@ let dayTime = document.querySelector("#date");
 
 dayTime.innerHTML = formatDate(date);
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+  let forecastHTML = `<div class= "row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+      <div class="weather-forcast-date">${day}</div>
+      <img
+        src="http://openweathermap.org/img/wn/50d@2x.png"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forcast-temperature">
+        <span class="max-temperature">18°</span>
+        <span class="min-temperature">12°</span>
+      </div>
+    </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
